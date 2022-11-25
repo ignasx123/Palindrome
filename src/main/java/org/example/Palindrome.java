8581e0ac70c460b97625bd325585ce84343e5d7d
Ignas Stepura <istepura@spartaglobal.com>
2022-11-25 11:49:57 +0000

    Second Test Commit

--- src/main/java/org/example/Palindrome.java
+++ src/main/java/org/example/Palindrome.java
@@ -7,7 +7,6 @@ public class Palindrome {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
 
         String n1 = "Whadup!!!";
         String n2 = new String("whadup!!!");
